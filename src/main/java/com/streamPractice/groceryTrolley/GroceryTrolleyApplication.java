3b927af7c4673764971c62c53748cdf76302c98e
Alexey Lavrinovich <lavrinleha95@gmail.com>
2023-04-07 01:05:52 -0700

    Init commit

---
+++ src/main/java/com/streamPractice/groceryTrolley/GroceryTrolleyApplication.java
@@ -0,0 +1,13 @@
+package com.streamPractice.groceryTrolley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryTrolleyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryTrolleyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/streamPractice/groceryTrolley/GroceryTrolleyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streamPractice.groceryTrolley;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryTrolleyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
